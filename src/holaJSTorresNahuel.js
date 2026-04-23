e9d6fb66d61434dad508a68e1cf794e4fefbc1f1
TorresNahuelTobias <tobiastorres123456@gmail.com>
2026-04-23 16:59:03 -0300

    agrego ni asistencia @Torres Nahuel Tobias

---
+++ src/holaJSTorresNahuel.js
@@ -0,0 +1,12 @@
+var nombre = "Tobias";
+
+let apellido = "Torres" ;
+
+const TEXTO = ' Hola Mundo, segun ${apellido} ${nombre} ';
+
+//console.log(TEXTO + nombre + apellido);
+// const TEXTO = 'hola mundo, ${apellido} ${nombre} ';
+//console.log("Hola mundo, segun " nombre + apellido);
+//console.log(TEXTO + nombre + apellido ;)
+
+console.log(TEXTO)
