4794124b2b827293ac971a42c6d49619cd3508c0
Esteban3748 <estebaneng22@gmail.com>
2026-04-30 17:37:32 -0300

    asistencia Sanchez Esteban

--- src/js/ejercicio20.js
+++ src/js/ejercicio20.js
@@ -1,2 +1,5 @@
-import { servicioTobiasTorres } from "../services/serviceTobiasTorres.js";
-import { servicioRiverosLuciano } from "../services/holaJSRiverosLuciano.js";
+
+
+
+
+import { serviceSanchezEsteban } from "../services/serviceSanchezEsteban"; 
