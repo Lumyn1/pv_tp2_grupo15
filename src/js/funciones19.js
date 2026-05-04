cee36563bd498117056171779dcfd42986dbeeb9
TorresNahuelTobias <tobiastorres123456@gmail.com>
2026-05-04 19:37:22 -0300

    correccion en funciones19.js @Torres Nahuel Tobias

--- src/js/funciones19.js
+++ src/js/funciones19.js
@@ -3,6 +3,6 @@ export const cambiarTexto = () => {
     const parrafo = document.getElementById("texto");
 
     parrafo.innerText = `El texto fue cambiado correctamente con JavaScript. 
-    Este ejercicio nos ayuda a entender cómo funcionan los eventos y la manipulación del DOM en una página web`;
+    Este ejercicio nos ayuda a entender cómo funcionan los eventos y la manipulación del DOM en una página web.`;
 
 };
