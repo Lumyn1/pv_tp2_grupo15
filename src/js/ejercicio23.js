e209c35d0e878b61ac00b3fbc7bf7083d12e1e59
EducanXD <educan1119@gmail.com>
2026-05-05 13:41:40 -0300

    Correccion del ejercicio 23 Cancino Arturo

--- src/js/ejercicio23.js
+++ src/js/ejercicio23.js
@@ -3,4 +3,11 @@ const textoMostrado = document.getElementById("textoMostrado");
 
 input.addEventListener("input", () => {
     textoMostrado.textContent = input.value;
+
+    if(input.value.length>20){
+        textoMostrado.classList.add("limite");
+    }
+    else{
+        textoMostrado.classList.remove("limite");
+    }
 });
